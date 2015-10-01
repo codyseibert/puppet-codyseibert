f6db0e3b6038479a45c3c51f81cb8342f513dd3e
Cody Seibert <cseibert113@gmail.com>
2015-09-30 22:13:54 -0400

    removing services

--- manifests/profile/mysql.pp
+++ manifests/profile/mysql.pp
@@ -1,18 +1,10 @@
 class codyseibert::profile::mysql (
 ) {
 
-  if defined (Service['firewalld']) == false {
-    service { 'firewalld':
-      ensure => 'latest',
-      provider => 'yum'
-    }
-  }
-
   if defined (Exec['firewall']) == false {
     exec { 'firewall':
       command => 'firewall-cmd --zone=public --add -port=3306/tcp --permanent',
       path => '/usr/bin',
-      notify => Service['firewalld'],
     }
   }
 
