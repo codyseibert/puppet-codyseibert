0c0ff1e60d85d870d385cbe2bce22c4493c36094
Cody Seibert <cseibert113@gmail.com>
2016-02-25 08:27:28 -0500

    updating mysql file

--- manifests/profile/mysql.pp
+++ manifests/profile/mysql.pp
@@ -1,40 +1,116 @@
 class codyseibert::profile::mysql (
 ) {
 
-  if defined (Exec['firewall']) == false {
-    exec { 'firewall':
-      command => 'firewall-cmd --zone=public --add-port=3306/tcp --permanent',
-      path => '/usr/bin',
-    }
-  }
+  # if defined (Exec['firewall']) == false {
+  #   exec { 'firewall':
+  #     command => 'firewall-cmd --zone=public --add-port=3306/tcp --permanent',
+  #     path => '/usr/bin',
+  #   }
+  # }
 
   class { '::mysql::server':
-    override_options => {
-      mysqld => { bind-address => '0.0.0.0'}
-    },
+    # override_options => {
+    #   mysqld => { bind-address => '0.0.0.0'}
+    # },
     databases   => {
-      'typr'  => {
+      'typer'  => {
         ensure  => 'present',
         charset => 'utf8',
       },
+      'linkr'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'snipr'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'pingr'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      },
+      'setter'  => {
+        ensure  => 'present',
+        charset => 'utf8',
+      }
     },
     grants => {
-      'typr@%/typr.*' => {
+      'typer@localhost/typer.*' => {
         ensure     => 'present',
         options    => ['GRANT'],
         privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
-        table      => 'typr.*',
-        user       => 'typr@%',
+        table      => 'typer.*',
+        user       => 'typer@%',
       },
+      'linkr@localhost/linkr.*' => {
+        ensure     => 'present',
+        options    => ['GRANT'],
+        privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+        table      => 'linkr.*',
+        user       => 'linkr@localhost',
+      },
+      'snipr@localhost/snipr.*' => {
+        ensure     => 'present',
+        options    => ['GRANT'],
+        privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+        table      => 'snipr.*',
+        user       => 'snipr@localhost',
+      },
+      'pingr@localhost/pingr.*' => {
+        ensure     => 'present',
+        options    => ['GRANT'],
+        privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+        table      => 'pingr.*',
+        user       => 'pingr@localhost',
+      },
+      'setter@localhost/setter.*' => {
+        ensure     => 'present',
+        options    => ['GRANT'],
+        privileges => ['SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+        table      => 'setter.*',
+        user       => 'setter@localhost',
+      }
     },
     users => {
-      'typr@%' => {
+      'typer@localhost' => {
         ensure                   => 'present',
         max_connections_per_hour => '0',
         max_queries_per_hour     => '0',
         max_updates_per_hour     => '0',
         max_user_connections     => '0',
-        password_hash            => '*94BDCEBE19083CE2A1F959FD02F964C7AF4CFC29',
+        password_hash            => '*341F4E7561D84BB46D4448AE5AFA38D98FCA4207',
+      },
+      'linkr@localhost' => {
+        ensure                   => 'present',
+        max_connections_per_hour => '0',
+        max_queries_per_hour     => '0',
+        max_updates_per_hour     => '0',
+        max_user_connections     => '0',
+        password_hash            => '*CC643370B2C9ED5D67805D848DC1DF1A69825D0A',
+      },
+      'snipr@localhost' => {
+        ensure                   => 'present',
+        max_connections_per_hour => '0',
+        max_queries_per_hour     => '0',
+        max_updates_per_hour     => '0',
+        max_user_connections     => '0',
+        password_hash            => '*59BE3DD02FF954CBA5344A3B25DAE39880BC793F',
+      },
+      'pingr@localhost' => {
+        ensure                   => 'present',
+        max_connections_per_hour => '0',
+        max_queries_per_hour     => '0',
+        max_updates_per_hour     => '0',
+        max_user_connections     => '0',
+        password_hash            => '*61B166487FB73A4FE2C97953EA6F9A0F1C2422E4',
+      },
+      'setter@localhost' => {
+        ensure                   => 'present',
+        max_connections_per_hour => '0',
+        max_queries_per_hour     => '0',
+        max_updates_per_hour     => '0',
+        max_user_connections     => '0',
+        password_hash            => '*F27940BD3A067E9FC3412E63D80A9EB12F528562',
       }
     }
   }
